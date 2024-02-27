19e0429b3987e95ae0ff373d4a62e64a93e0b15c
CatMaster <trabajo.nestor.098@gmail.com>
2024-02-27 17:48:06 +0100

    Actualizacion de la pagina web

--- js/index.js
+++ js/index.js
@@ -3,12 +3,30 @@ const navMenu = document.querySelector(".nav-menu")
 
 navToggle.addEventListener("click", () => {
     navMenu.classList.toggle("nav-menu-visible");
-
-    if (navMenu.classList.contains("nav-menu_visible")) {
+    
+    if (navToggle.firstElementChild.classList.contains("fa-x")) {
         navToggle.setAttribute("aria-label", "Cerrar menu");
+        navMenu.style.opacity = 0;
+        navToggle.firstElementChild.classList.remove("fa-x");
+        navToggle.firstElementChild.classList.add("fa-bars");
     } 
     else{
         navToggle.setAttribute("aria-label", "Abrir menu");
+        navMenu.style.opacity = 1;
+        navToggle.firstElementChild.classList.remove("fa-bars");
+        navToggle.firstElementChild.classList.add("fa-x");
     }
         
 });
+
+if(window.innerWidth <= 825){
+    navMenu.style.opacity = 0;
+}
+
+window.addEventListener("resize",()=>{
+    if(window.innerWidth > 825){
+        navMenu.style.opacity = 1;
+    }else{
+        navMenu.style.opacity = 0;
+    }
+})
